c296ba2662670687361e47547d302f6669edd7fd
mankyung <seomin1125@naver.com>
2024-01-27 15:14:06 +0900

    project1 생성함

---
+++ src/routes/project1/+page.svelte
@@ -0,0 +1,27 @@
+<script>
+    import image1 from '$assets/9.png';
+    import image2 from '$assets/2.jpeg';
+</script>
+
+<div class="w-full">
+    <img class="w-full"src={image1} alt="" />
+</div>
+
+
+<div class="flex flex-row m-6"> 
+    <div class="w-1/2" >
+        <p class="text-heading1 font-bold">Non-Human Agency</p>
+        <div>Hackney wick , London,UK<br />
+            Site area : 8,327 m2<br />
+            MA Royal College of Art 2012/2013<br />
+            Architecture ADS5_Urban Metabolism,Distinction<br />
+            Tutors : Jon Goodbun, Kenny Kinugasa-Tsui and Justin Lau
+        </div>
+    </div>
+
+    <div class="w-1/2">This project, Non-human agency, explores the potential of various near future smart materials, some of which might be 'grown' out of the complex material geology of post-industrial area Hackney Wick.
+        The semi-living research interface will harvest a new natural-polymer – tested here in real living prototypes – generating a new and valuable resource for the local community of post-industrial makers. Further research will explore hybrids of new smart materials and materials from existing local buildings and landscape. This bottom up approach questions traditional notions of architectural production as solely 'building', instead explores emerging ecologies of 'growing' as a new hybrid architectural language.</div>
+
+</div>
+
+<div></div>
